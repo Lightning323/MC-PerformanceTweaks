6aa44e6b6082fe003c6783d05417328a0f8fdf97
zipCoder933 <sam.p.w524@gmail.com>
2025-02-27 11:49:46 -0700

    initial commit

--- src/main/java/alternate/current/AlternateCurrentMod.java
+++ src/main/java/alternate/current/AlternateCurrentMod.java
@@ -3,16 +3,6 @@ package alternate.current;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import alternate.current.command.AlternateCurrentCommand;
-import alternate.current.util.profiler.ACProfiler;
-import alternate.current.util.profiler.Profiler;
-
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import org.zipcoder.performanceTweaks.PerfTweaks;
-
 /**
  * Taken from
  * https://github.com/SpaceWalkerRS/alternate-current/tree/forge
@@ -24,16 +14,13 @@ public class AlternateCurrentMod {
 	public static final String MOD_NAME = "Alternate Current";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 	public static final boolean DEBUG = false;
-	public static boolean on = true;
-	public static Profiler createProfiler() {
-		return DEBUG ? new ACProfiler() : Profiler.DUMMY;
-	}
+	public static boolean on = false;
 
-	@EventBusSubscriber(modid = PerfTweaks.MODID)
-	public static class ModEvents {
-		@SubscribeEvent
-		public static void onRegisterCommands(RegisterCommandsEvent event) {
-			AlternateCurrentCommand.register(event.getDispatcher());
-		}
-	}
+//	@EventBusSubscriber(modid = PerfTweaks.MODID)
+//	public static class ModEvents {
+//		@SubscribeEvent
+//		public static void onRegisterCommands(RegisterCommandsEvent event) {
+//			AlternateCurrentCommand.register(event.getDispatcher());
+//		}
+//	}
 }
