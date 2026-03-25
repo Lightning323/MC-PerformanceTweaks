25737ff75e4dd80b8fd7071007418a3ba36c1b70
Lightning323 <sam.p.w524@gmail.com>
2026-03-25 14:42:36 -0600

    changed from archetectury to multiloader

--- common/src/main/java/org/lightning323/performancetweaks/optimizations/server/redstone/interfaces/mixin/IServerLevel.java
+++
@@ -1,9 +0,0 @@
-package org.lightning323.performancetweaks.optimizations.server.redstone.interfaces.mixin;
-
-import org.lightning323.performancetweaks.optimizations.server.redstone.wire.WireHandler;
-
-public interface IServerLevel {
-
-	WireHandler alternate_current$getWireHandler();
-
-}
